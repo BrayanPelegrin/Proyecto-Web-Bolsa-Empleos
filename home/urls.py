ce37ad5fce07421a3a15da978349e223b513f677
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-10-08 17:17:37 -0400

    Loading template - Home

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+# Django
+from django.urls import path
+
+# Owns
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home')
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+# Django
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'index.html', {})
