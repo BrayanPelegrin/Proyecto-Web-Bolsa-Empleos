9d0a074c0fe73671c2a7dc8ecb171c428c5636fd
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-10-16 14:47:48 -0400

    Editing models

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class UserAccount(AbstractUser):
     email = models.EmailField(max_length=100, blank=True, null=True, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
     
