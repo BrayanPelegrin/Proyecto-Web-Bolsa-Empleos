46b70eb4313e95856998ad55418f85395b22d2da
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-10-16 10:28:35 -0400

    login and signup view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+# Django
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+# Owns 
+from .forms import SignupForm
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            username = username,
+            password = password
+        )
+
+        if user:
+            login(request, user)
+            messages.success(request, 'Welcome {}'.format(user.username))
+
+            if request.GET.get('next'):
+                return HttpResponseRedirect(request.GET['next'])
+
+            return redirect('home')
+        else:
+            messages.error(request, "Password or Username doesn't match")
+
+    return render(request, './login/login.html', {})
+
+
+def signup_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    form = SignupForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+
+        user = form.save()
+        if user:
+            login(request, user)
+            messages.success(request, 'User created succesfully')
+            return redirect('login')
+
+    context = {'form' : form}
+    return render(request, './signup/register.html', context)
