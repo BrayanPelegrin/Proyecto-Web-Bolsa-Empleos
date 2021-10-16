cf4570538a5902198ab036987341c6f3b18d4d62
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-10-16 10:27:46 -0400

    Creating UserAccount model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+# Django
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+class UserAccount(AbstractBaseUser):
+    """ User Account model """
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    objects =  UserManager()
