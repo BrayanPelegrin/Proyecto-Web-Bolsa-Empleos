f5f134262af2a785007c098802acd84fa221fb58
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-10-17 18:12:30 -0400

    Login Required at home

--- users/forms.py
+++ users/forms.py
@@ -7,9 +7,8 @@ class SignupForm(forms.Form):
         min_length = 2,
         max_length = 50,
         widget = forms.TextInput(attrs={
-            'class' : 'form-group',
+            'class' : 'form-control',
             'id' : 'username',
-            'placeholder' : 'example02',
             'aria-hidden' : 'true'
         })
     )
@@ -19,7 +18,7 @@ class SignupForm(forms.Form):
         min_length = 8,
         max_length = 100,
         widget = forms.PasswordInput(attrs = {
-            'class' : 'form-group',
+            'class' : 'form-control',
             'aria-hidden' : 'true'
         })
     )
